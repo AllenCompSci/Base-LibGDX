1427265eec3cfa633e4f6f340ed5b4b2556604a0
Allen Computer Science Club <taylor_hudson@allenisd.org>
2017-01-25 11:21:57 -0600

    Add files via upload

---
+++ libGDX - Base/desktop/src/com/compsci2/proj/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.compsci2.proj.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.compsci2.proj.lwjcGameCfg;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new lwjcGameCfg(), config);
+	}
+}
